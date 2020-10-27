6a93859dd227c63225b49ac64149f29df502dabb
Kevin S Hu <adm@Kevins-MacBook-Pro-2.local>
2020-10-27 18:44:18 -0500

    Register a service worker
    and fail to do so because we have no sw.js file to handle the
    installation step.

---
+++ index.js
@@ -0,0 +1,29 @@
+// Client-side JavaScript can change element colors
+const title = document.getElementById("title");
+title.style.color = "blue";
+
+// It can also register a service worker
+
+/**
+ * REGISTRATION
+ *
+ * tests for if navigator even has serviceWorker API
+ * if so, adds an event listener onto window's "load" event
+ */
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/sw.js").then(
+      function (registration) {
+        // Registration successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        // Registration failed
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
